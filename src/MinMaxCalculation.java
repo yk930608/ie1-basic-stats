c8fc7a69be4d3b1abb20a6ba4f097b6fe6dd435e
yk930608 <yk930608@gmail.com>
2023-10-01 17:25:41 -0400

    Added MinMaxCalculation.java"

---
+++ src/MinMaxCalculation.java
@@ -0,0 +1,12 @@
+import java.util.Collections;
+import java.util.List;
+
+public class MinMaxCalculation {
+    public Integer MinCalculation(List<Integer> nums) {
+        return Collections.min(nums);
+    }
+
+    public Integer MaxCalculation(List<Integer> nums) {
+        return Collections.max(nums);
+    }
+}
